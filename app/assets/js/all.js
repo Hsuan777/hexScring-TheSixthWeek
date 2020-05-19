94d1910b152f4a02b5dc7462b11533b34a57305d
Vic <Vic@gmail.com>
2020-05-19 11:33:28 +0800

    feat:產品導覽列

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,16 @@
 
 
 $(document).ready(() => {
-  console.log("hello");
+  
+  switch($('title').text()){
+    case "Craftsman":
+      $(".js__nav").addClass("fixed-top navbar-dark");
+      // $(".js__nav .nav-link").addClass("text-white");
+      
+      break;
+    case "ProductList":
+      // $(".js__nav .nav-link").addClass("text-dark");
+      $('a[href="productList.html"]').addClass('active');
+      break;
+  }
 });
