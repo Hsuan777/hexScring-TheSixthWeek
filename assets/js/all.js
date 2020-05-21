bea1ebbe7f0c623f8a11f883e62165e2c0862982
syuan0212@gmail.com <hsuan777>
2020-05-21 23:47:46 +0800

    Update 2020-05-21T15:47:37.863Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,29 +1,33 @@
+"use strict";
 
-
-$(document).ready(() => {
-  
-  switch($('title').text()){
+$(document).ready(function () {
+  switch ($('title').text()) {
     case "Craftsman":
       $(".js__nav").addClass("fixed-top navbar-dark");
-      
       break;
+
     case "ProductList":
       $('a[href="productList.html"]').addClass('active').removeClass('custom__text');
       break;
+
     case "Product":
       $('a[href="productList.html"]').addClass('active').removeClass('custom__text');
       break;
+
     case "Information":
       $('a[href="productList.html"]').removeClass('custom__text');
       break;
-  }
-  // 箭頭方向改變，提醒有折疊內容
+  } // 箭頭方向改變，提醒有折疊內容
+
+
   $(".js-btn ").click(function () {
     $(this).find(".js-btn__info").toggle();
-    if($(this).find(".js-btn___more").text()!="expand_less"){
+
+    if ($(this).find(".js-btn___more").text() != "expand_less") {
       $(this).find(".js-btn___more").text("expand_less");
-    }else{
+    } else {
       $(this).find(".js-btn___more").text("expand_more");
     }
   });
 });
+//# sourceMappingURL=all.js.map
