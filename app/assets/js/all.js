d4cc2bf84db365bedf2af66782b07e82ec48cd39
Vic <Vic@gmail.com>
2020-05-21 17:40:12 +0800

    fix:自訂義變數註解

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,5 +13,16 @@ $(document).ready(() => {
     case "Product":
       $('a[href="productList.html"]').addClass('active').removeClass('custom__text');
       break;
+    case "Information":
+      $('a[href="productList.html"]').removeClass('custom__text');
+      break;
   }
+  // 箭頭方向改變，提醒有折疊內容
+  $(".js-btn ").click(function () {
+    if($(this).find(".js-more").text()!="expand_less"){
+      $(".js-more").text("expand_less");
+    }else{
+      $(".js-more").text("expand_more");
+    }
+  });
 });
